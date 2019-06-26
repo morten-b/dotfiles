e4f76c9d936de38ee8c0feb07cf0cba027d7cff6
morten-b <mail@mbendtsen.dk>
2019-06-26 10:16:57 +0200

    sway

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
-# Start X at login
+# Start sway at login
 if status is-login
     if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
+	    export MOZ_ENABLE_WAYLAND=1
+	    export QT_QPA_PLATFORMTHEME=qt5ct
+	    export GTK_THEME=Arc-Dark-solid
+        exec sway
     end
 end
