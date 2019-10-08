d40406747b3b1ae1d8cb456dd9f4c127c113b680
morten-b <mail@mbendtsen.dk>
2019-10-08 12:44:19 +0200

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,11 +5,11 @@ if status is-login
 	export GTK_THEME=Arc-Dark-solid
 	export _JAVA_AWT_WM_NONREPARENTING=1 idea
 	export XDG_CURRENT_DESKTOP=Unity
-	#export PATH=$PATH:/home/morten/CompSys/x86prime/bin
+	export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/morten/CompSys/x86prime/bin
 	export TERM=xterm-256color
     exec sway -d 2> ~/.sway.log
     end
 end
 
 # opam configuration
-#source /home/morten/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+source /home/morten/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
