ba590f82daaa783c140fe4f8f4cf652f77bfbade
morten-b <mail@mbendtsen.dk>
2019-11-04 10:22:54 +0100

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,11 +6,12 @@ if status is-login
     if test -z "$DISPLAY" -a $XDG_VTNR = 1
 	export MOZ_ENABLE_WAYLAND=1
 	export GTK_THEME=Arc-Dark-solid
-	export _JAVA_AWT_WM_NONREPARENTING=1 idea
+	export _JAVA_AWT_WM_NONREPARENTING=1
 	export XDG_CURRENT_DESKTOP=Unity
-    export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/morten/CompSys/x86prime/bin
+    export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/morten/CompSys/x86prime/bin:/home/morten/maple2019/bin/
 	export TERM=xterm-256color
 	export EDITOR=micro
+	export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
     exec sway -d 2> ~/.sway.log
     end
 end
