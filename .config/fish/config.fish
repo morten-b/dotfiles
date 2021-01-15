bee519eee9bf9e46f2005b0624cc154a36ea4e1a
morten-b <32033374+morten-b@users.noreply.github.com>
2021-01-15 10:24:29 +0100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,8 @@ set -x WLR_NO_HARDWARE_CURSORS 1
 set -x MOZ_ENABLE_WAYLAND 1
 set -x XDG_CURRENT_DESKTOP sway
 set -x XDG_SESSION_TYPE wayland
+
+# Start Sway at login
+if test -z $DISPLAY; and test (tty) = "/dev/tty1"
+    sway
+end
