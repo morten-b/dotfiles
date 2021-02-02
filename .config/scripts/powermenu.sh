0d710fa9ff73e563669a245fd20cf5fe266c80ee
morten <mail@mbendtsen.dk>
2021-02-02 11:43:12 +0100

    first i3

--- .config/scripts/powermenu.sh
+++ .config/scripts/powermenu.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-entries="Logout Suspend Reboot Shutdown Lock"
+entries="' Logout Suspend Reboot Shutdown Lock"
 
-selected=$(printf '%s\n' $entries | wofi --conf=$HOME/.config/wofi/config.power --style=$HOME/.config/wofi/style.widgets.css | awk '{print tolower($1)}')
+#selected=$(printf '%s\n' $entries | wofi --conf=$HOME/.config/wofi/config.power --style=$HOME/.config/wofi/style.widgets.css | awk '{print tolower($1)}')
+selected=$(printf '%b\n' $entries | rofi -dmenu | awk '{print tolower($1)}')
 
 case $selected in
   logout)
