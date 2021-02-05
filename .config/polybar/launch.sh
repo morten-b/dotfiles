5085f9271c8d26e45f9c0e23c70573fb9d0e434e
morten <mail@mbendtsen.dk>
2021-02-05 14:53:15 +0100

    -

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+(
+  flock 200
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar primary 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
+  killall -q polybar
 
-echo "Bars launched..."
+  while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
+
+  outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  tray_output=eDP1
+
+  for m in $outputs; do
+    if [[ $m == "eDP1" ]]; then
+        tray_output=$m
+    fi
+  done
+
+  for m in $outputs; do
+    export MONITOR=$m
+    export TRAY_POSITION=none
+    if [[ $m == $tray_output ]]; then
+      TRAY_POSITION=right
+      polybar --reload primary </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+      disown
+    else
+      polybar --reload secondary </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+      disown 
+    fi
+  done
+) 200>/var/tmp/polybar-launch.lock
