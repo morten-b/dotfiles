12b85f065736e26d2f3a43dfbcd4138ecf30ebc6
Morten Skov Bendtsen <mosb@ascendispharma.com>
2024-12-08 17:17:08 +0100

    Update branch and remove hardware

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
