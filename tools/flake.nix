b9f50df0635e7a20111ad3638fdb2141ecb68f7d
Morten Skov Bendtsen <mosb@ascendispharma.com>
2025-01-03 16:28:07 +0100

    Testing

--- tools/flake.nix
+++ tools/flake.nix
@@ -1,16 +1,72 @@
-# in flake.nix
 {
-  inputs = {
-    # this is equivalent to `nixpkgs = { url = "..."; };`
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-  outputs = inputs: {
-    packages."x86_64-linux".default = derivation {
-      name = "simple";
-      builder = "${inputs.nixpkgs.legacyPackages."x86_64-linux".bash}/bin/bash";
-      args = [ "-c" "echo foo > $out" ];
-      src = ./.;
-      system = "x86_64-linux";
+  description = "Azure Functions Core Tools Package";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux"; # Adjust to your system, e.g., "aarch64-darwin" for macOS ARM
+      };
+    in {
+      packages.x86_64-linux.default = pkgs.callPackage ({ lib, stdenv, fetchFromGitHub, buildDotnetModule, buildGoModule, dotnetCorePackages, versionCheckHook }:
+        let
+          version = "4.0.6610";
+          src = fetchFromGitHub {
+            owner = "Azure";
+            repo = "azure-functions-core-tools";
+            tag = version;
+            hash = "sha256-tUNiyvIjaIrdo6377IdXND7YgIk9zKkazDHV4kiWYa8=";
+          };
+          gozip = buildGoModule {
+            pname = "gozip";
+            inherit version;
+            src = src + "/tools/go/gozip";
+            vendorHash = null;
+          };
+        in
+        buildDotnetModule {
+          pname = "azure-functions-core-tools";
+          inherit src version;
+
+          dotnet-sdk = dotnetCorePackages.sdk_8_0;
+          dotnet-runtime = dotnetCorePackages.sdk_8_0;
+          dotnetFlags = [ "-p:TargetFramework=net8.0" ];
+          nugetDeps = ./deps.json;
+          useDotnetFromEnv = true;
+          executables = [ "func" ];
+
+          postPatch = ''
+            substituteInPlace src/Azure.Functions.Cli/Common/CommandChecker.cs \
+              --replace-fail "CheckExitCode(\"/bin/bash" "CheckExitCode(\"${stdenv.shell}"
+          '';
+
+          postInstall = ''
+            mkdir -p $out/bin
+            ln -s ${gozip}/bin/gozip $out/bin/gozip
+          '';
+
+          meta = {
+            homepage = "https://github.com/Azure/azure-functions-core-tools";
+            description = "Command line tools for Azure Functions";
+            mainProgram = "func";
+            license = lib.licenses.mit;
+            maintainers = with lib.maintainers; [
+              mdarocha
+              detegr
+            ];
+            platforms = [
+              "x86_64-linux"
+              "aarch64-darwin"
+              "x86_64-darwin"
+            ];
+          };
+        } {});
+      
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = [
+          self.packages.x86_64-linux.default
+        ];
+      };
     };
-  };
 }
