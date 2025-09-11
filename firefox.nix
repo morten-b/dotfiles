7a541a010a26e7c4e2c1ab60ef09da54e2333809
Morten Skov Bendtsen <mosb@ascendispharma.com>
2025-09-11 09:02:04 +0200

    Seperate firefox

---
+++ firefox.nix
@@ -0,0 +1,91 @@
+{
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      default = {
+        id = 0;
+        name = "default";
+        isDefault = true;
+        search = {
+          force = true;
+          default = "google";
+          order = [ "google" ];
+          engines = {
+            "google".metaData.alias = "@g";
+          };
+        };
+      };
+    };
+    # https://mozilla.github.io/policy-templates/
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      FirefoxHome = {
+        Search = false;
+        TopSites = false;
+        SponsoredTopSites = false;
+        Highlights = false;
+        Pocket = false;
+        SponsoredPocket = false;
+        Snippets = false;
+        Locked = true;
+      };
+      FirefoxSuggest = {
+        WebSuggestions = false;
+        SponsoredSuggestions = false;
+        ImproveSuggest = false;
+        Locked = false;
+      };
+      DNSOverHTTPS = {
+        Enabled = true;
+        ProviderURL = "https://dns.nextdns.io/2a7136";
+        Locked = true;
+        Fallback = false;
+      };
+      AutofillCreditCardEnabled = false;
+      TranslateEnabled = false;
+      PasswordManagerEnabled = false;
+      OfferToSaveLogins = false;
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = false;
+      DisableFirefoxScreenshots = true;
+      DisableSetDesktopBackground = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "never";
+      SearchBar = "unified";
+      NoDefaultBookmarks = true;
+
+      # Check about:support for extension/add-on ID strings.
+      # Valid strings for installation_mode are "allowed", "blocked",
+      # "force_installed" and "normal_installed".
+      ExtensionSettings = {
+        #"*".installation_mode = "blocked";
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "@testpilot-containers" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/multi-account-containers/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "floccus@handmadeideas.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/floccus/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+  };
+}
