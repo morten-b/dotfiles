b57d7129ba3fb0a41c16f73a1d9013c41c24d6f4
Morten Skov Bendtsen <mosb@ascendispharma.com>
2025-09-17 18:24:13 +0200

    Testing

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         (final: prev: {
           gnome-keyring = prev.gnome-keyring.overrideAttrs (oldAttrs: {
             mesonFlags = (builtins.filter (flag: flag != "-Dssh-agent=true") oldAttrs.mesonFlags) ++ [
-              "-Dssh-agent=false"
+              "-Dssh-agent=true"
             ];
           });
         })
