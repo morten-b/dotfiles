95c384cb84a4f6ebc07c06b8031e1da11a018509
Morten Skov Bendtsen <mosb@ascendispharma.com>
2025-09-17 18:19:12 +0200

    Testing

--- overlays/chromium.nix
+++ overlays/chromium.nix
@@ -2,8 +2,7 @@ final: prev: {
   chromium = prev.chromium.overrideAttrs (_: {
     postInstall = ''
       echo "Removing chromium-browser.desktop"
-      rm -f $out/share/applications/chromium-browser.desktop
-      echo "Removing chromium-browser.desktop"
+      #rm -f $out/share/applications/chromium-browser.desktop
     '';
   });
 }
