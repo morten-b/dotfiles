9f24f4c2a4adf71b209912a22506dffd7354fd28
Morten Skov Bendtsen <mosb@ascendispharma.com>
2025-12-08 11:45:32 +0100

    Refactor agenix configuration: comment out mobilepay environment secrets and systemd tmpfiles rules

--- agenix.nix
+++ agenix.nix
@@ -1,17 +1,14 @@
 { config, pkgs, lib, ... }:
-let
-  homePathExists = builtins.pathExists /home/morten;
-in
 {
   age.identityPaths = [ "/home/morten/.ssh/id_rsa" ];
 
   environment.systemPackages = [ pkgs.ragenix ];
 
-  systemd.tmpfiles.rules = lib.optionals homePathExists [
-    "d /home/morten/repos 0755 morten users -"
-    "d /home/morten/repos/Borum 0755 morten users -"
-    "d /home/morten/repos/Borum/mobilepay 0755 morten users -"
-  ];
+  # systemd.tmpfiles.rules = [
+  #   "d /home/morten/repos 0755 morten users -"
+  #   "d /home/morten/repos/Borum 0755 morten users -"
+  #   "d /home/morten/repos/Borum/mobilepay 0755 morten users -"
+  # ];
 
   age.secrets = {
     wg-preshared-key = {
@@ -43,19 +40,18 @@ in
       owner = "morten";
       group = "users";
     };
-  } // lib.optionalAttrs homePathExists {
-    mobilepay-env-prod = {
-      file = ./secrets/mobilepay-env-prod.age;
-      owner = "morten";
-      group = "users";
-      path = "/home/morten/repos/Borum/mobilepay/.env-prod";
-    };
+    # mobilepay-env-prod = {
+    #   file = ./secrets/mobilepay-env-prod.age;
+    #   owner = "morten";
+    #   group = "users";
+    #   path = "/home/morten/repos/Borum/mobilepay/.env-prod";
+    # };
 
-    mobilepay-env-test = {
-      file = ./secrets/mobilepay-env-test.age;
-      owner = "morten";
-      group = "users";
-      path = "/home/morten/repos/Borum/mobilepay/.env-test";
-    };
+    # mobilepay-env-test = {
+    #   file = ./secrets/mobilepay-env-test.age;
+    #   owner = "morten";
+    #   group = "users";
+    #   path = "/home/morten/repos/Borum/mobilepay/.env-test";
+    # };
   };
 }
