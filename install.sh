14ff06206507bf73177965d09ed8163540eecb21
Morten Skov Bendtsen <mosb@ascendispharma.com>
2025-12-08 12:38:54 +0100

    WIP

--- install.sh
+++ install.sh
@@ -9,7 +9,10 @@ git clone --branch disko https://github.com/morten-b/dotfiles.git
 cd dotfiles
 
 # Run disko
-yes | sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount disk-config-T490-ext4.nix
+echo ""
+echo "WARNING: This will destroy all data on /dev/nvme0n1"
+read -p "Press Enter to continue or Ctrl+C to abort..."
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount disk-config-T490-ext4.nix
 
 # Install NixOS
 cd /tmp/dotfiles
