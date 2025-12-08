495b0289fa2f3297cf2ef3717cdd18747e753661
Morten Skov Bendtsen <mosb@ascendispharma.com>
2025-12-08 12:36:58 +0100

    Fix command syntax in install script for disko run

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ git clone --branch disko https://github.com/morten-b/dotfiles.git
 cd dotfiles
 
 # Run disko
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --yes-wipe-all-disks --mode destroy,format,mount disk-config-T490-ext4.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount --yes-wipe-all-disks disk-config-T490-ext4.nix
 
 # Install NixOS
 cd /tmp/dotfiles
