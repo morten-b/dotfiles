614f2fa3e2f4170202a2b123a761240df272dfc7
Morten Skov Bendtsen <mosb@ascendispharma.com>
2025-12-08 08:36:08 +0100

    Refactor hardware configuration by enabling kvm-intel kernel module and removing unused settings

--- hardware-configuration-T490.nix
+++ hardware-configuration-T490.nix
@@ -10,32 +10,8 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/8eafbd7c-8611-477e-81cd-9955f0447993";
-      fsType = "ext4";
-    };
-
-  boot.initrd.luks.devices."luks-f1fd9eae-5a0f-480f-a396-0dab183d2fe3".device = "/dev/disk/by-uuid/f1fd9eae-5a0f-480f-a396-0dab183d2fe3";
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2D43-73B6";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wwp0s20f0u7.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
