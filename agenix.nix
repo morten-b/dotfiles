7c803d56276f2162e2306df88a693e6c95df3231
Morten Skov Bendtsen <mosb@ascendispharma.com>
2025-12-08 10:59:36 +0100

    Reorganize agenix configuration: uncomment mobilepay secrets and systemd tmpfiles rules; restructure age.secrets

--- agenix.nix
+++ agenix.nix
@@ -1,56 +1,61 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let
+  homePathExists = builtins.pathExists /home/morten;
+in
 {
   age.identityPaths = [ "/home/morten/.ssh/id_rsa" ];
 
   environment.systemPackages = [ pkgs.ragenix ];
 
-  # systemd.tmpfiles.rules = [
-  #   "d /home/morten/repos 0755 morten users -"
-  #   "d /home/morten/repos/Borum 0755 morten users -"
-  #   "d /home/morten/repos/Borum/mobilepay 0755 morten users -"
-  # ];
+  systemd.tmpfiles.rules = lib.optionals homePathExists [
+    "d /home/morten/repos 0755 morten users -"
+    "d /home/morten/repos/Borum 0755 morten users -"
+    "d /home/morten/repos/Borum/mobilepay 0755 morten users -"
+  ];
 
-  age.secrets.wg-preshared-key = {
-    file = ./secrets/wg-preshared-key.age;
-    owner = "morten";
-    group = "users";
-  };
+  age.secrets = {
+    wg-preshared-key = {
+      file = ./secrets/wg-preshared-key.age;
+      owner = "morten";
+      group = "users";
+    };
 
-  age.secrets.wg-private-key = {
-    file = ./secrets/wg-private-key.age;
-    owner = "morten";
-    group = "users";
-  };
+    wg-private-key = {
+      file = ./secrets/wg-private-key.age;
+      owner = "morten";
+      group = "users";
+    };
 
-  age.secrets.tailscale-auth-key = {
-    file = ./secrets/tailscale-auth-key.age;
-    owner = "morten";
-    group = "users";
-  };
+    tailscale-auth-key = {
+      file = ./secrets/tailscale-auth-key.age;
+      owner = "morten";
+      group = "users";
+    };
 
-  # age.secrets.mobilepay-env-prod = {
-  #   file = ./secrets/mobilepay-env-prod.age;
-  #   owner = "morten";
-  #   group = "users";
-  #   path = "/home/morten/repos/Borum/mobilepay/.env-prod";
-  # };
+    jellyfin-admin = {
+      file = ./secrets/jellyfin-admin.age;
+      owner = "morten";
+      group = "users";
+    };
 
-  # age.secrets.mobilepay-env-test = {
-  #   file = ./secrets/mobilepay-env-test.age;
-  #   owner = "morten";
-  #   group = "users";
-  #   path = "/home/morten/repos/Borum/mobilepay/.env-test";
-  # };
+    jellyfin-jellyfin = {
+      file = ./secrets/jellyfin-jellyfin.age;
+      owner = "morten";
+      group = "users";
+    };
+  } // lib.optionalAttrs homePathExists {
+    mobilepay-env-prod = {
+      file = ./secrets/mobilepay-env-prod.age;
+      owner = "morten";
+      group = "users";
+      path = "/home/morten/repos/Borum/mobilepay/.env-prod";
+    };
 
-  age.secrets.jellyfin-admin = {
-    file = ./secrets/jellyfin-admin.age;
-    owner = "morten";
-    group = "users";
-  };
-
-  age.secrets.jellyfin-jellyfin = {
-    file = ./secrets/jellyfin-jellyfin.age;
-    owner = "morten";
-    group = "users";
+    mobilepay-env-test = {
+      file = ./secrets/mobilepay-env-test.age;
+      owner = "morten";
+      group = "users";
+      path = "/home/morten/repos/Borum/mobilepay/.env-test";
+    };
   };
 }
